6e137bebf019cb1c527f15bcce7c90fe00d0eff1
kimhotac <rlaghxkr@Gmail.com>
2024-10-25 12:12:33 +0900

    id 연결 실수오류 정정

--- testHeat/src/index.js
+++ testHeat/src/index.js
@@ -4,7 +4,7 @@ import * as address from './address'
 document.addEventListener('DOMContentLoaded', () => {
 
     const imgCaseButton = document.getElementById('toimg'); // 이미지 버튼
-    const heatCaseButton = document.getElementById('heatmap'); // 텍스트 버튼
+    const heatCaseButton = document.getElementById('toheatmap'); // 텍스트 버튼
     const textCaseButton = document.getElementById('totext'); // 텍스트 버튼
     const idxCaseButton = document.getElementById('toindex'); // 로고
     
