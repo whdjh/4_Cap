0bd54e281008b08d986ac7b983a3c9e1731af9a3
kimhotac <rlaghxkr@Gmail.com>
2024-10-27 19:31:54 +0900

    홈화면 로고버튼 쿼리 삭제

--- testHeat/src/index.js
+++ testHeat/src/index.js
@@ -6,11 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const imgCaseButton = document.getElementById('toimg'); // 이미지 버튼
     const heatCaseButton = document.getElementById('toheatmap'); // 텍스트 버튼
     const textCaseButton = document.getElementById('totext'); // 텍스트 버튼
-    const idxCaseButton = document.getElementById('toindex'); // 로고
-    
-    if (idxCaseButton) {
-        idxCaseButton.addEventListener('click', address.handleLinkClickToIdx);
-    }
+
     if (heatCaseButton) {
         heatCaseButton.addEventListener('click', address.handleLinkClickFromIdx);
     }
