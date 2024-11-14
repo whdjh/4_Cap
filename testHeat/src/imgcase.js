679dd322975ce7529c91ee60d7021ed8cdd88c58
whdjh <99352927+whdjh@users.noreply.github.com>
2024-11-14 23:51:30 +0900

    Update imgcase.js

--- testHeat/src/imgcase.js
+++ testHeat/src/imgcase.js
@@ -3,7 +3,9 @@ import * as heatmap from "./draw-heatmap";
 import * as address from "./address";
 import GazEmo from "./gazeEmoClass";
 
-const licenseKey = "dev_fafdh08rb5wsibob5c1xy5nm7wpjdc26alecpx2l";
+/* 키값입력
+const licenseKey = "";
+*/
 const redirectUrl = "http://localhost:8082/imgcase.html";
 export const gazEmo = new GazEmo(licenseKey, redirectUrl);
 
