a203d72e7b7d29e307a689820956e8ea2a996e02
whdjh <whdjh98@naver.com>
2024-11-14 23:44:10 +0900

    최종

--- Main/src/index.js
+++
@@ -1,8 +0,0 @@
-// import GazEmo from './gazeEmoClass.js';
-import * as adress from './address.js';
-
-document.querySelectorAll('.btn.watch-btn, .btn.cali-btn').forEach((button) => {
-    button.addEventListener('click', () => {
-        adress.checkCali
-    });
-});
